c2a08697b7e257e63ca3079ab1d306bda016aa3a
unknown <mrlnrose6@gmail.com>
2023-11-27 10:16:00 +0530

    new Array

--- src/com/oranium/java/ObjectArray.java
+++ src/com/oranium/java/ObjectArray.java
@@ -4,7 +4,7 @@ public class ObjectArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("New Array");
 		
 		Object a[] = {1,1.2,"java",'l'};
 		
